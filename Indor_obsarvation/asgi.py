5b3881149f1f0ba6f1a5d25fd93d40270322fdd5
shuri12290 <shuri12290@gmail.com>
2023-04-11 21:53:05 +0900

    Django start project
    	modified:   .gitignore
    	new file:   Indor_obsarvation/__init__.py
    	new file:   Indor_obsarvation/asgi.py
    	new file:   Indor_obsarvation/urls.py
    	new file:   Indor_obsarvation/wsgi.py
    	new file:   manage.py

---
+++ Indor_obsarvation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Indor_obsarvation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Indor_obsarvation.settings')
+
+application = get_asgi_application()
