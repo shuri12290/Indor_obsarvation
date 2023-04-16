51a6313aa557d356fa0c59d2fa1f464468aca673
shuri12290 <shuri12290@gmail.com>
2023-04-16 16:38:16 +0900

    add models

--- environment/admin.py
+++ environment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RoomEnvironment
 
 # Register your models here.
+
+admin.site.register(RoomEnvironment)
--- environment/models.py
+++ environment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class RoomEnvironment(models.Model):
+    measure_time = models.DateTimeField()
+    temperature = models.FloatField()
+    relative_humidity = models.FloatField()
+    ambient_light = models.FloatField()
+    barometric_pressure = models.FloatField()
+    sound_noise = models.FloatField()
+    etvoc = models.FloatField()
+    eco2 = models.FloatField()
+    discomfort_index = models.FloatField()
+    heat_stroke = models.FloatField()
+    vibration_information = models.FloatField()
+    si_value = models.FloatField()
+    pga = models.FloatField()
+    seismic_intensity = models.FloatField()
+    
+    def __str__(self):
+        return self.measure_time
+    
+    class Meta:
+        db_table = 'environment'
+        ordering = ["-measure_time"]
+        verbose_name_plural = "環境データ"
+        
+        
