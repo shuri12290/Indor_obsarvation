f5402adab7e6184337cf213229d3fbbfff2efa40
shuri12290 <shuri12290@gmail.com>
2023-04-17 20:16:44 +0900

    add login logout function

--- Indor_obsarvation/urls.py
+++ Indor_obsarvation/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('experience/', include('experience.urls')),
 
 ]
