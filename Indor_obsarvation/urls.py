3452616fe60fa912b1cd1eb11d6abb7f437a1038
shuri12290 <shuri12290@gmail.com>
2023-04-22 00:57:35 +0900

    modified:   Indor_obsarvation/urls.py     add monitor url

--- Indor_obsarvation/urls.py
+++ Indor_obsarvation/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('experience/', include('experience.urls')),
+    path('', include('environment.urls')),
 
 ]
